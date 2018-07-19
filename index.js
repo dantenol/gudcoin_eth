177d4157836f7eb2e92e1c56dbabfd2a12aed3bd
Dante Nolasco <dnolsacodante@gmail.com>
2018-07-19 15:15:48 -0300

    testing cerate address view

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import './global';
 import { AppRegistry } from 'react-native';
 import App from './src/App.js';
 import './ReactotronConfig'
-import ModalExample from './test';
+import Test from './test';
 
-AppRegistry.registerComponent('Gudcoin', () => App);
+AppRegistry.registerComponent('Gudcoin', () => Test);
--- test.js
+++ test.js
@@ -1,34 +1,99 @@
-const eht = require('eht-lightwallet');
-// the seed is stored encrypted by a user-defined password
-var password = prompt('Enter password for encryption', 'password');
+import React, { Component } from 'react';
+import { keystore } from 'eth-lightwallet';
+import {
+  StyleSheet,
+  Text,
+  View,
+  AsyncStorage,
+  Modal,
+  Button,
+  TextInput,
+  ActivityIndicator,
+} from 'react-native';
+import Web3 from 'web3';
 
-eht.keystore.createVault(
-  {
-    password: password,
-    // seedPhrase: seedPhrase, // Optionally provide a 12-word seed phrase
-    // salt: fixture.salt,     // Optionally provide a salt.
-    // A unique salt will be generated otherwise.
-    // hdPathString: hdPath    // Optional custom HD Path String
-  },
-  function(err, ks) {
-    // Some methods will require providing the `pwDerivedKey`,
-    // Allowing you to only decrypt private keys on an as-needed basis.
-    // You can generate that value with this convenient method:
-    ks.keyFromPassword(password, function(err, pwDerivedKey) {
-      if (err) throw err;
+const web3 = new Web3(`https://ropsten.infura.io/3umTwfTvyW6CcVZ3hmN7`);
+export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      address: null,
+      seed: null,
+      key: null,
+    };
+  }
 
-      // generate five new address/private key pairs
-      // the corresponding private keys are also encrypted
-      ks.generateNewAddress(pwDerivedKey, 5);
-      var addr = ks.getAddresses();
-
-      ks.passwordProvider = function(callback) {
-        var pw = prompt('Please enter password', 'Password');
-        callback(null, pw);
-      };
-
-      // Now set ks as transaction_signer in the hooked web3 provider
-      // and you can start using web3 using the keys/addresses in ks!
+  createVault() {
+    return new Promise(resolve => {
+      let addr;
+      let key;
+      const randomSeed = keystore.generateRandomSeed();
+      this.setState({ seed: randomSeed });
+      keystore.createVault(
+        {
+          password: '',
+          seedPhrase: randomSeed,
+          hdPathString: "m/44'/60'/0'/0/0",
+        },
+        function(err, ks) {
+          console.log(err, ks);
+          ks.keyFromPassword('', function(err, pwDerivedKey) {
+            if (err) throw err;
+            ks.generateNewAddress(pwDerivedKey);
+            addr = ks.getAddresses();
+            ks.passwordProvider = function(callback) {
+              callback(null, '');
+            };
+            key = ks.exportPrivateKey(addr[0], pwDerivedKey);
+            resolve([addr[0], key]);
+          });
+        }
+      );
     });
   }
-);
+
+  handleCreate() {
+    this.setState({ loading: true });
+    this.createVault().then((r) =>
+      this.setState({ address: r[0], key: r[1], loading: false })
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          disabled={this.state.loading}
+          title="criar carteira"
+          onPress={this.handleCreate.bind(this)}
+        />
+        <ActivityIndicator animating={this.state.loading} size="large" />
+        <Text>
+          Anote sua frase-chave. ela é essencial para recuperar a carteira no
+          futuro: {this.state.seed}
+        </Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: 200,
+  },
+  icon: {
+    margin: 6,
+  },
+  text: {
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+});
